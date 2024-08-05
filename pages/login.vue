84dac78d6a18d1e6fbe3867955a86c3d71e77f19
asliddinberdiev <asliddinwork@gmail.com>
2024-08-05 11:04:25 +0500

    edit width

--- pages/login.vue
+++ pages/login.vue
@@ -1,50 +1,55 @@
 <script setup lang="ts">
-import {useLogin} from "@/composables/useAuth";
+import { useLogin } from "@/composables/useAuth";
 
 definePageMeta({
   layout: "auth",
 });
 
-const {username, password, loading, submit} = useLogin()
-
+const { username, password, loading, submit } = useLogin();
 </script>
 
 <template>
-  <v-card class="card-border-radius bg-grey-lighten-3 px-4 py-2" rounded="xl">
-    <v-card-title class="font-weight-bold text-uppercase mb-4 text-center">System Of Furqat</v-card-title>
+  <v-card
+    class="card-border-radius bg-grey-lighten-3 px-4 py-2 w-full"
+    min-width="320"
+    rounded="xl"
+  >
+    <v-card-title class="font-weight-bold text-uppercase mb-4 text-center"
+      >System Of Furqat</v-card-title
+    >
     <form @submit.prevent="submit">
       <v-text-field
-          v-model="username.value.value"
-          :error-messages="username.errorMessage.value"
-          rounded="xl"
-          bg-color="grey-lighten-3"
-          variant="solo"
-          placeholder="username"
-          min-width="320px"
-          prepend-inner-icon="mdi-account-outline"
+        v-model="username.value.value"
+        :error-messages="username.errorMessage.value"
+        rounded="xl"
+        bg-color="grey-lighten-3"
+        variant="solo"
+        placeholder="username"
+        prepend-inner-icon="mdi-account-outline"
       />
       <v-text-field
-          v-model="password.value.value"
-          :error-messages="password.errorMessage.value"
-          :disabled="!!username.errorMessage.value || !username.value.value"
-          rounded="xl"
-          bg-color="grey-lighten-3"
-          variant="solo"
-          placeholder="password"
-          min-width="320px"
-          prepend-inner-icon="mdi-lock-outline"
-          type="password"
+        v-model="password.value.value"
+        :error-messages="password.errorMessage.value"
+        :disabled="!!username.errorMessage.value || !username.value.value"
+        rounded="xl"
+        bg-color="grey-lighten-3"
+        variant="solo"
+        placeholder="password"
+        prepend-inner-icon="mdi-lock-outline"
+        type="password"
       />
 
       <v-btn
-          :loading="loading"
-          :disabled="!!password.errorMessage.value || !password.value.value || loading"
-          class="my-4 text-white"
-          color="cyan"
-          size="large"
-          rounded="xl"
-          block
-          type="submet"
+        :loading="loading"
+        :disabled="
+          !!password.errorMessage.value || !password.value.value || loading
+        "
+        class="my-4 text-white"
+        color="cyan"
+        size="large"
+        rounded="xl"
+        block
+        type="submet"
       >
         Login
       </v-btn>
@@ -54,7 +59,6 @@ const {username, password, loading, submit} = useLogin()
 
 <style>
 .card-border-radius {
-  box-shadow: -5px -5px 12px white,
-  5px 5px 10px rgba(0, 0, 0, 0.23) !important;
+  box-shadow: -5px -5px 12px white, 5px 5px 10px rgba(0, 0, 0, 0.23) !important;
 }
 </style>
