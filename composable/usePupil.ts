26b6737738ce904326e725e2e7c480f40a4758dc
asliddinberdiev <asliddinwork@gmail.com>
2024-08-08 18:15:22 +0500

    -

--- composable/usePupil.ts
+++ composable/usePupil.ts
@@ -42,7 +42,7 @@ export function usePupilForm() {
 
   const isOpenModal = ref(false);
   const userImageFile = ref();
-  const phoneNumberRegex = /^998(20|33|77|88|90|91|93|94|97|98|99)\d{7}$/;
+  const phoneNumberRegex = /^998(20|33|50|55|77|88|90|91|93|94|95|97|98|99)\d{7}$/;
   // schema
   const schema = {
     firstname(value: string): boolean | string {
